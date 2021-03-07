b943a4ca60f3e4dc60f137237eceb249b4fdb3bf
Chris Szczechowicz <info@cjszczechowicz.com>
2021-03-06 19:18:26 -0500

    Clean up the spacing

--- sketch.js
+++ sketch.js
@@ -1,58 +1,55 @@
 // TODO: Create setup() function
 
-  // Inside the setup() function:
-  // TODO: Create canvas 500px wide and 500px high
+// Inside the setup() function:
+// TODO: Create canvas 500px wide and 500px high
 
-  // TODO: Draw horizontal and vertical guidelines
-
+// TODO: Draw horizontal and vertical guidelines
 
 
 // TODO: Create wall drawing inside draw() function
 
-  // Inside the draw() function:
-  // Draw parallel lines:
-  // TODO: Set stroke color and weight
+// Inside the draw() function:
+// Draw parallel lines:
+// TODO: Set stroke color and weight
 
 
 
-  // TODO: Use a for loop to draw 10 vertical lines
+// TODO: Use a for loop to draw 10 vertical lines
 
 
 
-  // Draw polka dots:
-  // TODO: Set no stroke and fill color
+// Draw polka dots:
+// TODO: Set no stroke and fill color
 
 
 
-  // TODO: Use nested for loops to draw a grid of circles
-  // TODO: Offset y positions of every other column by 10 pixels
+// TODO: Use nested for loops to draw a grid of circles
+// TODO: Offset y positions of every other column by 10 pixels
 
 
 
-  // Draw checkered squares:
-  // TODO: Set fill color
+// Draw checkered squares:
+// TODO: Set fill color
 
 
-  // TODO: Use nested for loops to draw rows of squares
-  // TODO: Offset y positions of every other column by 25 pixels
-
-
-  
-  // Draw parallel diagonal lines:
-  // TODO: Set stroke color and weight
-
-
-  // TODO: Use a for loop to draw diagonal lines
+// TODO: Use nested for loops to draw rows of squares
+// TODO: Offset y positions of every other column by 25 pixels
 
 
 
-  // Draw borders:
-  // TODO: Set stroke color
+// Draw parallel diagonal lines:
+// TODO: Set stroke color and weight
 
 
-  // TODO: Draw horizontal and vertical guidelines
+// TODO: Use a for loop to draw diagonal lines
 
 
-  // TODO: Draw borders around canvas
+
+// Draw borders:
+// TODO: Set stroke color
 
 
+// TODO: Draw horizontal and vertical guidelines
+
+
+// TODO: Draw borders around canvas
